72a0873bfc882e8a96364b841cc086bc95c8f6be
James Mountain <james.w.mountain@gmail.com>
2017-07-12 16:23:09 +0000

    Added pokemon damage type command (atktype, deftype, optional 2nd type) Added type info to folder

---
+++ commands/project/pokemon.js
@@ -0,0 +1,49 @@
+const Commando = require('discord.js-commando');
+
+module.exports = class PokemonCommando extends Commando.Command {
+    constructor(client) {
+        super(client, {
+            name:         "pokemon",
+            group:        "project",
+            memberName:   "pokemon",
+            description:  "Calculates the damage type multiplier of pokemon types.",
+            args: [
+                {
+                    key: "atktype",
+                    label: "atktype",
+                    prompt: "Attack type of the attacking pokemon's move.",
+                    type: "string"
+                },
+                {
+                    key: "dmgtype",
+                    label: "dmgtype",
+                    prompt: "Damage type 1 of the defending pokemon.",
+                    type: "string"
+                },
+                {
+                    key: "dmgtype2",
+                    label: "dmgtype2",
+                    prompt: "Damage type 2 of the defending pokemon (optional)",
+                    type: "string",
+                    default: "none"
+                }
+            ]
+        });
+    };
+
+    async run(msg, args) {
+        let typm = this.client.typemultipliers
+        let typna = this.client.typenames
+
+        function getMultiAttackDmgIndex(atktype, deftype) {
+            return typm[typna.findIndex(v => v === atktype)][typna.findIndex(v => v === deftype)];
+        }
+
+        let multi = getMultiAttackDmgIndex(args.atktype, args.dmgtype);
+        if (args.dmgtype2 !== "none") {
+            multi *= getMultiAttackDmgIndex(args.atktype, args.dmgtype2);
+        }
+
+        return msg.reply(multi + "x")
+    }
+};
--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const Discord = require('discord.js');
 const Commando = require('discord.js-commando');
 const path = require('path');
 const token = require('./auth').token
+const XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+const fs = require('fs');
 
 const client = new Commando.Client({
     owner: '334612943230599168'
@@ -12,6 +14,54 @@ client.on('ready', () => {
 });
 
 client.persons = []
+client.typenames = [
+    "normal",
+    "steel",
+    "rock",
+    "fire",
+    "fairy",
+    "bug",
+    "dragon",
+    "electric",
+    "dark",
+    "fighting",
+    "flying",
+    "ghost",
+    "grass",
+    "ground",
+    "ice",
+    "poison",
+    "psychic",
+    "water"
+];
+client.typemultipliers = [];
+
+let reqs = [];
+function loadTypesFromFile() {
+    let typn = client.typenames
+
+    for (let i = 0; i < typn.length; i++) {
+        var url = "./typedata/" + typn[i] + ".json";
+
+        let data = JSON.parse(fs.readFileSync(url, 'utf8'))
+
+        var currepdmgrels = data.damage_relations;
+        client.typemultipliers[i] = Array(typn.length).fill(1);
+
+        currepdmgrels.half_damage_to.forEach(function(typ) {
+            client.typemultipliers[i][typn.findIndex(v => v === typ.name)] = 0.5;
+        })
+
+        currepdmgrels.no_damage_to.forEach(function(typ) {
+            client.typemultipliers[i][typn.findIndex(v => v === typ.name)] = 0;
+        })
+
+        currepdmgrels.double_damage_to.forEach(function(typ) {
+            client.typemultipliers[i][typn.findIndex(v => v === typ.name)] = 2;
+        })
+    }
+}
+loadTypesFromFile();
 
 client.registry
     .registerGroups([
