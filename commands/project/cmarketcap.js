41d700561cd0d6c2544180ed364761af0a6b90bb
James Mountain <james.w.mountain@gmail.com>
2017-07-12 13:03:02 +0000

    Added a command to print a fizz buzz display. Added a command to display the price of a cryptocurrency based on it's proper name.

---
+++ commands/project/cmarketcap.js
@@ -0,0 +1,41 @@
+const Commando = require('discord.js-commando');
+const XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+
+module.exports = class CoinMarketCapCommand extends Commando.Command {
+    constructor(client) {
+        super(client, {
+            name:         "coinprice",
+            group:        "project",
+            memberName:   "coinprice",
+            description:  "Gets the USD price of a particular cryptocurrency.",
+            args: [
+                {
+                    key: "currency",
+                    label: "currency",
+                    prompt: "The currency name to get USD price for.",
+                    type: "string"
+                }
+            ]
+        });
+    };
+
+    async run(msg, args) {
+        let url = "https://api.coinmarketcap.com/v1/ticker/" + args.currency + "/"
+        let request = new XMLHttpRequest();
+        request.open("GET", url);
+        request.responseType = 'json';
+        request.send();
+
+        request.onload = function() {
+            let data = JSON.parse(request.responseText)
+
+            if (data.error) {
+                msg.reply("Could not find coin.")
+            } else {
+                msg.reply("Current price of " + args.currency + " : $" + data[0].price_usd)
+            }
+        }
+
+        return msg.reply("Searching for price...")
+    }
+};
---
+++ commands/project/fizzbuzz.js
@@ -0,0 +1,51 @@
+const Commando = require('discord.js-commando');
+
+module.exports = class FizzBuzzCommand extends Commando.Command {
+    constructor(client) {
+        super(client, {
+            name:         "fizzbuzz",
+            group:        "project",
+            memberName:   "fizzbuzz",
+            description:  "Replaces multiples of 3 and 5 with a word in sequence of numbers.",
+            args: [
+                {
+                    key: "number",
+                    label: "number",
+                    prompt: "The number you would like to perform fizz buzz up to.",
+                    type: "integer"
+                },
+                {
+                    key: "fizz",
+                    label: "fizz",
+                    prompt: "The word you'd like to replace multiples of 3 with.",
+                    type: "string"
+                },
+                {
+                    key: "buzz",
+                    label: "buzz",
+                    prompt: "The word you'd like to replace multiples of 5 with.",
+                    type: "string"
+                }
+            ]
+        });
+    };
+
+    async run(msg, args) {
+        let buildString = ""
+        for (var i = 1; i <= args.number; i++) {
+            if (i % 3 == 0) {
+                buildString += args.fizz
+            }
+            if (i % 5 == 0) {
+                buildString += args.buzz
+            }
+            if (i % 3 != 0 && i % 5 != 0) {
+                buildString += i
+            }
+
+            buildString += " | "
+        }
+
+        return msg.reply(buildString)
+    }
+};
